733abb758b8f8679dfbcc42aca743b077ede1702
anas <anasxonerkinjonov@gmail.com>
2024-04-10 21:33:55 +0500

    some changes

--- main.cpp
+++ main.cpp
@@ -5,29 +5,44 @@
 using namespace std;
 
 class Solution {
-public:
-    bool isValid(string s) {
-        vector<int> stack;
-        size_t lenght = s.length();
-        for (size_t i = 0; i < lenght; i++){
-            char c = s[i];
-            if (stack.size() != 0 && (c - stack.back() == 1 || c - stack.back() == 2)){
-                stack.pop_back();
-            } else {
-                stack.push_back(c);
+   public:
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1, result = nums[0];
+
+        while (l <= r) {
+            if (nums[l] < nums[r]) {
+                result = min(result, nums[l]);
+                break;
             }
+
+            int mid = l + (r - l) / 2;
+            result = min(result, nums[mid]);
+
+            if (nums[l] <= nums[mid])
+                l = mid + 1;
+            else
+                r = mid - 1;
         }
-        return stack.empty();
+
+        return result;
     }
 };
 
 int main() {
     Solution* s = new Solution();
 
-    string input;
-    cin >> input;
+    int size;
+    cin >> size;
 
-    auto res = s->isValid(input);
+    vector<int> inputs;
+
+    for (int i = 0; i < size; i++) {
+        int j;
+        cin >> j;
+        inputs.push_back(j);
+    }
+
+    auto res = s->findMin(inputs);
 
     cout << endl
          << res << endl;
