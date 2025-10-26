7d6a310bfbc71fcf5e0334c4e0eddc6ebfff238b
Anas <anaserkinjonov@gmail.com>
2025-10-26 21:46:32 +0500

    ¯\_(ツ)_/¯

---
+++ src/main/kotlin/p0046_permutations/solution.kt
@@ -0,0 +1,28 @@
+package p0046_permutations
+
+class Solution {
+    fun permute(nums: IntArray): List<List<Int>> {
+        val result = ArrayList<List<Int>>()
+        val subset = ArrayList<Int>()
+        val state = BooleanArray(nums.size)
+
+        fun backtrack(){
+            if (subset.size == nums.size) {
+                result.add(subset.toList())
+                return
+            }
+            for (j in nums.indices){
+                if (state[j]) continue
+                subset.add(nums[j])
+                state[j] = true
+                backtrack()
+                subset.removeLast()
+                state[j] = false
+            }
+        }
+
+        backtrack()
+
+        return result
+    }
+}
---
+++ src/main/kotlin/p0973_k_closest_points_to_origin/solution.kt
@@ -0,0 +1,11 @@
+package p0973_k_closest_points_to_origin
+
+import java.util.*
+
+class Solution {
+    fun kClosest(points: Array<IntArray>, k: Int): Array<IntArray> {
+        val heap = PriorityQueue(compareBy<IntArray> { it[0] * it[0] + it[1] * it[1] })
+        points.forEach { heap.add(it) }
+        return Array(k) { heap.poll() }
+    }
+}
