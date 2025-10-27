d413ceb8b70e57d24c2b7bdb8f02bedd965e70ae
Anas <anaserkinjonov@gmail.com>
2025-10-27 21:55:45 +0500

    ¯\_(ツ)_/¯

---
+++ src/main/kotlin/p0017_letter_combinations_of_a_phone_number/solution.kt
@@ -0,0 +1,25 @@
+package p0017_letter_combinations_of_a_phone_number
+
+class Solution {
+    fun letterCombinations(digits: String): List<String> {
+        if (digits.isEmpty()) return emptyList()
+
+        val res = ArrayList<String>(3 * digits.length)
+        val digitToChar = arrayOf("abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz")
+
+        val sb = CharArray(digits.length)
+        fun backtrack(i: Int) {
+            if (i == digits.length) {
+                res.add(String(sb))
+                return
+            }
+            for (c in digitToChar[digits[i].code - 50]) {
+                sb[i] = c
+                backtrack(i + 1)
+            }
+        }
+
+        backtrack(0)
+        return res
+    }
+}
